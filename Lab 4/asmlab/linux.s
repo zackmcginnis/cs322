afc562b4c981e0b10dc1305643c79375ef213974
Zack McGinnis <zm3@pdx.edu>
2015-04-23 08:00:54 -0700

    lab 4
    updates

---
+++ Lab 4/asmlab/linux.s
@@ -0,0 +1,11 @@
+	.file	"linux.s"     # change this to match your filename
+	.text
+	.globl	f
+f:
+### This is where your code begins ...
+
+        movl    $42, %eax     # replace this with your code!
+
+### This is where your code ends ...
+
+	ret
---
+++ Lab 4/asmlab/mac.s
@@ -0,0 +1,11 @@
+	.file	"mac.s"       # change this to match your filename
+        .text
+	.globl	_f
+_f:
+### This is where your code begins ...
+
+        movl    $42, %eax     # replace this with your code!
+
+### This is where your code ends ...
+
+	ret
