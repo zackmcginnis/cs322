284c1a59f4da48120c700a42bef4c7a701128b66
Zack McGinnis <zm3@pdx.edu>
2015-04-28 13:39:42 -0700

    hw update
    update

--- Lab 4/hw2/program1.s
+++ Lab 4/hw2/program1.s
@@ -3,9 +3,25 @@
 	.globl	f
 f:
 
-### This is where your code begins ...
 
-        movl    $42, %eax     # replace this with your code!
+###    For each of the following tasks, you are asked to write a fragment of
+###    x86-64 code that takes the starting addresses of two arrays, represented
+###    as above, in the rdi and rsi registers as its input and returns a single
+###    integer as its result in eax.
+
+###    Program 1: The result should be the total value of the numbers in the
+###	   two arrays, not including the length fields at the start of
+###	   each array.  The two arrays themselves should not be
+###	   modified.
+
+        movl    $0, %eax        # initialize length count in eax
+        jmp     test
+loop:   incl    %eax            # increment count
+        addq    $4, %rdi        # and move to next array element
+
+test:   movl    (%rdi), %ecx    # load array element
+        cmpl    $0, %ecx        # test for end of array
+        jne     loop            # repeat if we're not done ...
 
 ### This is where your code ends ...
 
