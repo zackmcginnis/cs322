17392401f82f9c0929b7e14e53def9269973a354
Zack McGinnis <zm3@pdx.edu>
2015-04-28 14:05:15 -0700

    hw change
    p1 update

--- Lab 4/hw2/program1.s
+++ Lab 4/hw2/program1.s
@@ -14,14 +14,24 @@ f:
 ###	   each array.  The two arrays themselves should not be
 ###	   modified.
 
-        movl    $0, %eax        # initialize length count in eax
-        jmp     test
-loop:   incl    %eax            # increment count
-        addq    $4, %rdi        # and move to next array element
+         movl    $0, %eax        # initialize length count in eax
+         movl    $0, %ecx        # initialize length count in ecx
+         jmp     test1
+loop1:   incl    %eax            # increment count
+         addq    $4, %rdi        # and move to next array element
 
-test:   movl    (%rdi), %ecx    # load array element
-        cmpl    $0, %ecx        # test for end of array
-        jne     loop            # repeat if we're not done ...
+test1:   movl    1(%rdi), %eax    # load array element
+         cmpl    $0, %eax        # test for end of array
+         jne     loop1            # repeat if we're not done ...
+
+loop2:   incl    %ecx            # increment count
+         addq    $4, %rsi        # and move to next array element
+
+test2:   movl    1(%rsi), %ecx    # load array element
+         cmpl    $0, %ecx        # test for end of array
+         jne     loop2            # repeat if we're not done ...
+
+         addl    %eax, %ecx      # add both array counts, store in eax
 
 ### This is where your code ends ...
 
