2064a523187be46572fadbeebac86125134935e2
Zack McGinnis <zm3@pdx.edu>
2015-05-01 15:05:26 -0700

    ..
    ..

--- Lab 4/hw2/program2.s
+++ Lab 4/hw2/program2.s
@@ -9,8 +9,8 @@ f:
 ### This is where your code begins ...
 
         movl    $0, %eax        # eax will count the number of elements
-        #movl    $0, %rdi
-        #movl    $0, %rsi
+        movq    $0, %rdi
+        movq    $0, %rsi
         movl    (%rdi), %edx    # read array element of first byte which signifies length
         movl    (%rsi), %ebx
 
