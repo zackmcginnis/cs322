e6f0d36230cbead8ee9465bc43650cb2488a9222
Zack McGinnis <zm3@pdx.edu>
2015-05-01 13:11:56 -0700

    hw2
    update

--- Lab 4/hw2/program1.s
+++ Lab 4/hw2/program1.s
@@ -14,55 +14,30 @@ f:
 ###	   each array.  The two arrays themselves should not be
 ###	   modified.
 
-         movl    $0, %eax        # initialize total in eax
-         movl    $0, %ecx        # initialize total in ecx
-         jmp     test1
-loop1:   movl    (%rdi), %eax    # eax will record the running total
-         addq    $4, %rdi        # and move to next array element
+        movl    (%rdi), %eax    # eax will record the running total
 
-test1:   movl    (%rdi), %eax    # load array element
-         cmpl    $0, %eax        # test for end of array
-         jne     loop1
-         jmp     test2            # repeat if we're not done ...
+loop:   addq    $4, %rdi        # move to next element in the array
+        movl    (%rdi), %edx    # read array element
+        cmpl    $0, %edx        # are we done?
+        jmp     loop1
+        addl    %edx, %eax      # add to running total
+        jmp     loop
 
-loop2:   movl    (%rsi), %ecx    # ecx will record the running total
-         addq    $4, %rsi        # and move to next array element
+loop1:  movl    (%rsi), %eax    # eax will record the running total
+        hmp     loop2
 
-test2:   movl    (%rsi), %ecx    # load array element
-         cmpl    $0, %ecx        # test for end of array
-         jne     loop2            # repeat if we're not done ...
+loop2:  addq    $4, %rsi        # move to next element in the array
+        movl    (%rsi), %edx    # read array element
+        cmpl    $0, %edx        # are we done?
+        je      done
+        addl    %edx, %eax      # add to running total
+        jmp     loop2
 
-         addl    %ecx, %eax      # add both array counts, store in eax
-
-
-
-
-
-
-#     movl    $1, %ecx        # ecx will count the number of elements
-#        movl    (%rdi), %eax    # eax will record the running total
-#
-#loop:   addq    $4, %rdi        # move to next element in the array
- #       movl    (%rdi), %edx    # read array element
- #       cmpl    $0, %edx        # are we done?
- #       je      done
- #       addl    %edx, %eax      # add to running total
-#        incl    %ecx            # increment count
- #       jmp     loop
 
         # When we reach this point in the program, %eax will
         # contain the total of all the array elements and ecx
         # contain the number of array elements.
-
-
-
-
-
-
-
-
-
-
+done:
 
 ### This is where your code ends ...
 
