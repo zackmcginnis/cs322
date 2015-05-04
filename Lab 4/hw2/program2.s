1e80a572194a36366ac21a4fad7a336c3cdaa21c
Zack McGinnis <zm3@pdx.edu>
2015-05-04 11:47:41 -0700

    ..
    ..

--- Lab 4/hw2/program2.s
+++ Lab 4/hw2/program2.s
@@ -12,10 +12,9 @@ f:
         movq    $0, %rdi
         movq    $0, %rsi
         movl    (%rdi), %edx    # read array element of first byte which signifies length
-        movl    (%rsi), %e8
 
 loop1:  cmpl    $0, %edx
-        je      loop2
+        je      loop2a
         decl    %edx            #decrement the value of the length of the array (to know how many values to count)
         addq    $4, %rdi        # move to next element in the array
         movl    (%rdi), %ecx    # read array element
@@ -26,12 +25,16 @@ loop1:  cmpl    $0, %edx
 incloop1: incl  %eax
           jmp   loop1
 
-loop2:  cmpl    $0, %e8
+loop2a: movl    (%rsi), %edx
+        jmp l   loop2
+
+
+loop2:  cmpl    $0, %edx
         je      done
-        decl    %e8            #decrement the value of the length of the array (to know how many values to count)
+        decl    %edx           #decrement the value of the length of the array (to know how many values to count)
         addq    $4, %rsi        # move to next element in the array
-        movl    (%rsi), %e9    # read array element
-        cmpl    $0, %e9        # are we done?
+        movl    (%rsi), %ecx    # read array element
+        cmpl    $0, %ecx        # are we done?
         je      loop2
         jmp     incloop2
 
