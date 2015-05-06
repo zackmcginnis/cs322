c5d174b1bc7931ff6da961ae0328931b67309674
Zack McGinnis <zm3@pdx.edu>
2015-05-06 11:14:46 -0700

    .
    .

--- Lab 4/hw2/program3.s
+++ Lab 4/hw2/program3.s
@@ -15,7 +15,6 @@ loop1:  cmpl    $0, %edx
         je      loop2
         decl    %edx
         addq    $4, %rdx        # move to next array element
-        movl    (%rdx), %eax
         jmp     loop1
 
 
