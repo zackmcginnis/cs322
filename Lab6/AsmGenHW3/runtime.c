fab2353a2e8ef2adef1654ee2cd9093dd20f7e4c
Zack McGinnis <zm3@pdx.edu>
2015-05-21 11:39:52 -0700

    .
    .

--- Lab6/AsmGenHW3/runtime.c
+++ Lab6/AsmGenHW3/runtime.c
@@ -4,11 +4,11 @@
 //to XinitGlobals() when you have implemented support for
 //initializing global variables.
 
-//extern void XinitGlobals();
+extern void XinitGlobals();
 extern void Xmain();
 
 int main(int argc, char** argv) {
-    //XinitGlobals();
+    XinitGlobals();
     Xmain();
     return 0;
 }
@@ -16,8 +16,30 @@ int main(int argc, char** argv) {
 void Xprint(int val) {
     printf("output: %d\n", val);
 }
+/*
+void XinitGlobals() {
+                      
 
-//void XinitGlobals() {
-  
-//}
+        for (int i=0; i<defns.length; i++) {
+            defns[i].compileFunction(a, globals);
+        }
 
+            // Initialize global variables:
+        for (int i=0; i<vars.length; i++) {
+           env = vars[i].declareGlobals(a, env);
+        }
+        return env;
+  }
+
+      void compileFunction(Assembly a, LocEnv globals) {
+        a.emit(".globl", a.name(name));
+        a.emitLabel(a.name(name));
+        a.emitPrologue();
+        Frame f = new FunctionFrame(formals, globals);
+        //f.dump(a);
+        if (body.compile(a, f)) {
+            a.emitEpilogue();
+        }
+        a.emit();
+    }
+*/
