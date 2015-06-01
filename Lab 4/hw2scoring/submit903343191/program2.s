8f2f5be02078a1fd25c910d04b0fd560458a540f
Zack McGinnis <zm3@pdx.edu>
2015-05-31 18:15:40 -0700

    .
    .

---
+++ Lab 4/hw2scoring/submit903343191/program2.s
@@ -0,0 +1,48 @@
+	.file	"program2.s"
+        .text
+	.globl	f
+f:
+
+#Program 2: The result should be the total number of zeros in the first
+#	   array plus the total number of nonzeros in the second array.
+
+### This is where your code begins ...
+
+        movl    $0, %eax        # eax will count the number of elements
+        #movq    $0, %rdi
+        #movq    $0, %rsi
+        movl    (%rdi), %edx    # read array element of first byte which signifies length
+
+loop1:  cmpl    $0, %edx
+        je      loop2a
+        decl    %edx            #decrement the value of the length of the array (to know how many values to count)
+        addq    $4, %rdi        # move to next element in the array
+        movl    (%rdi), %ecx    # read array element
+        cmpl    $0, %ecx        # are we done?
+        je      incloop1
+        jmp     loop1
+
+incloop1: incl  %eax
+          jmp   loop1
+
+loop2a: movl    (%rsi), %edx
+        jmp     loop2
+
+
+loop2:  cmpl    $0, %edx
+        je      done
+        decl    %edx           #decrement the value of the length of the array (to know how many values to count)
+        addq    $4, %rsi        # move to next element in the array
+        movl    (%rsi), %ecx    # read array element
+        cmpl    $0, %ecx        # are we done?
+        je      loop2
+        jmp     incloop2
+
+incloop2: incl  %eax
+          jmp   loop2
+
+done:
+
+### This is where your code ends ...
+
+	ret
--- Lab 4/hw2scoring/submit903343191/program3.s
+++ Lab 4/hw2scoring/submit903343191/program3.s
@@ -6,12 +6,12 @@ f:
 #Program 3: The result should be 1 and the contents of the first array
 #	   should be reversed.  The second array should not be changed.
 ### This is where your code begins ...
-
-        movq    $0, %rdx
-      
-        movl    $0, %eax
-	movl    $0, %ecx
-	movl    $0, %edx
+   
+        
+		movl    $0, %eax
+		movl    $0, %edx
+		movq    $0, %rdx
+		movl    $0, %ecx
         movq    %rdi, %rdx
         movl    (%rdi), %edx             # initialize %edx at start of array      
         addq    $4, %rdi
@@ -30,18 +30,18 @@ loop1:  cmpl    $0, %edx
 ### process stops when %rdi >= %rdx, at which point we can be sure
 ### that the array has been reversed:
 
-loop2:  cmpq    %rdi, %rdx      # compare pointers at two ends of array              
+loop2:  cmpq    %rdx, %rdi      # compare pointers at two ends of array              
         jnl     done
         movl    (%rdi), %ecx    # read values from each end of the array
-        movl    (%rdx), %eax
-        movl    %eax, (%rdi)    # write them back in reverse order
+        movl    (%rdx), %edx
+        movl    %edx, (%rdi)    # write them back in reverse order
         movl    %ecx, (%rdx)
         addq    $4, %rdi        # adjust pointers at each end of array
         subq    $4, %rdx
         jmp     loop2           # and repeat ...
 
-done:   #movl    (%rdi), %eax 
-        movl    $1,  %eax
+done:   movl    $1,  %eax
+        
         # the problem description doesn't specify what value should be
         # returned in %eax so we won't worry about it here ...
 
