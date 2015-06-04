627a42090d4c5711e248dc564b2ecfdf8a273135
Zack McGinnis <zm3@pdx.edu>
2015-06-04 13:30:20 -0700

    f
    f

--- Lab8/hw4/TestHeap4c.java
+++ Lab8/hw4/TestHeap4c.java
@@ -1,24 +1,22 @@
 class TestHeap4c {
-    static final int S = 100;
-        public static void main(String[] args) {
-        Heap h = Heap.make(S);
-        // allocate space for two objects on the heap
-        h.a = h.alloc(1);
-        h.b = h.alloc(1);
+  static final int S = 100;
 
-        // store a pointer to the second object in the first object
-        h.store(h.a, 1, h.b);
+  public static void main(String[] args) {
+    Heap h = Heap.make(S);
+    h.a = h.alloc(1);
+    h.b = h.alloc(1);
 
-        //store a pointer to the first object in the second
-        h.store(h.b, 1, h.a);
+    h.store(h.a, 1, h.b);
+    h.store(h.b, 1, h.a);
 
-        System.out.println("Before garbage collection;");
-        h.dump();
-        System.out.println("Free space remaining = " + h.freeSpace());
+    System.out.println("Before garbage collection;");
+    h.dump();
+    System.out.println("Free space remaining = " + h.freeSpace());
 
-        h.garbageCollect();
-        System.out.println("After garbage collection;");
-        h.dump();
-        System.out.println("Free space remaining = " + h.freeSpace());
-    }
+    h.garbageCollect();
+    
+    System.out.println("After garbage collection;");
+    h.dump();
+    System.out.println("Free space remaining = " + h.freeSpace());
+  }
 }
